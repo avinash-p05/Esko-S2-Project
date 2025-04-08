ae8712fbf0337a2533be9c5c20bd0bbfa25fd6fb
avinash-p05 <git21cs100-t@students.git.edu>
2025-04-08 14:21:45 +0530

    refactored the code

--- src/main/java/com/eskocloud/Esko/S2/Demo/controller/home.java
+++ src/main/java/com/eskocloud/Esko/S2/Demo/controller/home.java
@@ -1,19 +1,25 @@
 package com.eskocloud.Esko.S2.Demo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 @CrossOrigin(origins = "http://localhost:5173/")
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public class home {
 
-    @GetMapping("get-message")
-    public String sayHello(){
-        return "hello world";
+    @GetMapping("/get-message")
+    public ResponseEntity<Map<String,String>> sayHello(){
+        Map<String,String> response = new TreeMap<>();
+        response.put("code","ok");
+        response.put("message","Hi from Backend!");
+        return ResponseEntity.ok(response);
     }
 
 }
