e3f8282d97b358949047c70cdba22cd662dc56ab
avinash-p05 <git21cs100-t@students.git.edu>
2025-04-08 12:40:54 +0530

    first commit

---
+++ src/main/java/com/eskocloud/Esko/S2/Demo/EskoS2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.eskocloud.Esko.S2.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EskoS2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EskoS2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eskocloud/Esko/S2/Demo/EskoS2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskocloud.Esko.S2.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskoS2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
